62e2cbeb14178171092797598e5cf8df8e3bc322
Greg Allen <me@jga.me>
2012-11-07 08:35:32 -0800

    added grunt

---
+++ grunt.js
@@ -0,0 +1,60 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    info: '<json:package.json>',
+    meta: {
+      banner: '/*!\n'+
+              ' * <%= info.name %> - <%= info.description %>\n'+
+              ' * v<%= info.version %>\n'+
+              ' * <%= info.homepage %>\n'+
+              ' * copyright <%= info.copyright %> <%= grunt.template.today("yyyy") %>\n'+
+              ' * <%= info.license %> License\n'+
+              '*/'
+    },
+    lint: {
+      all: ['grunt.js', 'lib/*.js', 'test/*.test.js']
+    },
+    clientside: {
+      main: {
+        main: 'lib/thrive.js',
+        name: 'Thrive',
+        output: 'dist/thrive.js'
+      }
+    },
+    concat: {
+      dist: {
+        src: ['<banner>', 'dist/thrive.js'],
+        dest: 'dist/thrive.js'
+      }
+    },
+    min: {
+      dist: {
+        src: ['<banner>', 'dist/thrive.js'],
+        dest: 'dist/thrive.min.js'
+      }
+    },
+    simplemocha: {
+      all: {
+        src: 'test/**/*.test.js',
+        options: {
+          ui: 'tdd',
+          reporter: 'list',
+          growl: true
+        }
+      }
+    },
+    watch: {
+      js: {
+        files: '<config:lint.all>',
+        tasks: 'default' 
+      }
+    },
+    server:{
+      port: 8000,
+      base: '.'
+    }
+  });
+  grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-clientside');
+  grunt.registerTask('default', 'lint clientside concat min simplemocha');
+  grunt.registerTask('dev', 'default server watch');
+};
